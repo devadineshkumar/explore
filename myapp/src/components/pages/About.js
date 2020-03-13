2ec013410fb1b2c97af408d0febee959ab356f14
devadineshkumar <devadineshkumar@gmail.com>
2020-03-13 17:52:29 +0530

    React test app git commit.

---
+++ myapp/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p> this is the todo list app v1.1.1.
+                It is just add to do list and delete and mark complete</p>
+            
+      </React.Fragment>
+    )
+}
