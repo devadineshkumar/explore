5c43afa9af08b86ab6b313524493e813b3384b11
devadineshkumar <devadineshkumar@gmail.com>
2020-04-08 13:18:39 +0530

    zsdx

--- angular/OnlineStore/src/app/admin/OrderTable.component.ts
+++ angular/OnlineStore/src/app/admin/OrderTable.component.ts
@@ -1,10 +1,30 @@
+import { OrderRepository } from './../../Model/order.repository';
+import { Order } from './../../Model/order.model';
 import { Component } from '@angular/core';
-
 
 @Component({
     templateUrl: './OrderTable.component.html'
 })
-
 export class OrderTableComponent {
+
+    includeShipped: boolean = false;
+
+
+    constructor(public repository: OrderRepository) {
+
+    }
+
+    getOrders() : Order[] {   
+        return this.repository.getOrders().filter( o => this.includeShipped || !o.shipped);
+    }
+
+    markShipped(order: Order) {
+        order.shipped = true;
+        this.repository.updateOrder(order);
+    }
+
+    deleteOrder(id: number) {
+        this.repository.deleteOrder(id);
+    }
 
 }
